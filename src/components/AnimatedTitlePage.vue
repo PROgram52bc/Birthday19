1350a47836f6264d359404c44c858e69755deb84
David <daviddenghaotian@163.com>
2019-01-09 22:30:36 -0500

    Added glowing effect to button

--- src/components/AnimatedTitlePage.vue
+++ src/components/AnimatedTitlePage.vue
@@ -6,14 +6,16 @@
 		</div>
 		<div class="lower-container">
 			<div class="button" ref="button" v-on:click="$emit('start-button-clicked')">
-				<svg height="25vh" version="1.1" viewBox="0 0 97 111" xmlns="http://www.w3.org/2000/svg">
-					<g transform="translate(-58 -89)">
-					<path transform="scale(-1,1)" d="m-62 196-88-51 88-51z" fill="#3cb8d7" fill-rule="evenodd" stroke="#7f7f7f" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="1" stroke-width="9" style="paint-order:normal"/>
-					</g>
-				</svg>
-			</div>
-		</div>
-	</div>
+                <svg height="30vh" version="1.1" viewBox="0 0 99 113" xmlns="http://www.w3.org/2000/svg">
+                    <g transform="translate(2 0)">
+                    <g transform="translate(-58,-89)" fill="#f1f6f7" stroke="#7f7f7f">
+                    <path transform="scale(-1,1)" d="m-62 196-88-51 88-51z" fill="#f1f6f7" fill-rule="evenodd" stroke="#7f7f7f" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="1" stroke-width="9" style="paint-order:normal"/>
+                    </g>
+                    </g>
+                </svg>
+            </div>
+        </div>
+    </div>
 </template>
 <script>
 // eslint-disable-next-line
@@ -116,25 +118,35 @@ By default, this component displays nothing, until the appear() method is called
 	justify-content: center;
 	font-family: Tahoma, Helvetica, Arial, "Microsoft Yahei","微软雅黑", STXihei, "华文细黑", sans-serif;
 	flex-shrink: 2;
-	color: white;
+	color: transparent;
 }
 .main-title {
 	padding: 10px;
 	text-shadow: 0 0 2px white, 0 0 5px white, 0 0 7px white, 0 0 40px #3cb8d7, 0 0 70px #3cb8d7, 0 0 80px #3cb8d7, 0 0 100px #3cb8d7;
-	font-size: 8vmax;
+	font-size: 8vh;
 	margin-top: 1em;
 	margin-bottom: 1em;
 }
 .sub-title {
 	text-shadow: 0 0 2px white, 0 0 5px white, 0 0 7px white, 0 0 40px #ffc0cb, 0 0 70px #ffc0cb, 0 0 80px #ffc0cb, 0 0 100px #ffc0cb;
-	font-size: 7vmax;
+	font-size: 7vh;
 }
 .button {
 	display: flex;
 	justify-content: center;
 	cursor: pointer;
 	flex-shrink: 3;
-	text-shadow: 0 0 2px white, 0 0 5px white, 0 0 7px white, 0 0 40px #ffc0cb, 0 0 70px #ffc0cb, 0 0 80px #ffc0cb, 0 0 100px #ffc0cb;
+}
+.button svg {
+    filter: 
+    drop-shadow(0 0 2px white)
+    drop-shadow(0 0 5px white)
+    drop-shadow(0 0 7px white)
+    drop-shadow(0 0 20px white)
+    drop-shadow(0 0 40px white)
+    /*
+    drop-shadow(0 0 100px white);
+    */
 }
 
 </style>
